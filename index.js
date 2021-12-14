a65a0963ef93b1274de382b2ca76dffda2ccb607
ed <cabrale234@gmail.com>
2021-12-14 11:49:58 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+require("dotenv").config()
+const PORT = 8000;
+const express = require('express');
+const bcrypt = require('bcrypt')
+const { v1: uuidv1} = require('uuid')
+const StreamChat = require('stream-chat').StreamChat
+const app = express()
+const cors = require('cors'); 
+const { connect } = require('getstream');
+app.use(cors())
+app.use(express.json())
+
+const { API_KEY, API_SECRET, APP_ID} = process.env
+
+
+// Sign up
+app.post('/signup', async (req, res) => {
+    try {
+        const { username, password} = req.body
+        const userId = uuidv1()
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const client = connect(API_KEY, API_SECRET, APP_ID)
+        const token = client.createUserToken(userId)
+
+        res.status(200).json({ username, userId, hashedPassword, token})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: error})
+    }
+})
+
+app.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body
+        const client = connect(API_KEY, API_SECRET, APP_ID)
+        const chatClient = StreamChat.getInstance(API_KEY,API_SECRET)
+        const {users} = await chatClient.queryUsers({name: username})
+
+        if (!username.length) return res.json(400).json({ message: 'user does not exist'})
+
+        const success = await bcrypt.compare(password, users[0].hashedPassword)
+        const token = client.createUserToken(users[0].id)
+        const confirmedName = users[0].name
+        const userId = users[0].id
+        if (success) {
+            res.status(200).json({token, username: confirmedName, userId })
+        } else {
+            res.status(500).res.json({message: 'Login failed'})
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: error})
+    }
+})
+
+app.listen(PORT, () => console.log('Server running on PORT ' + PORT));
